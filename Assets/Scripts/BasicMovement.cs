9e2c535f1de6a202aa10b4acaee5ef4a77dcb0cc
Semin Choi <schoi369@gatech.edu>
2020-02-03 13:27:58 -0500

    Initial Commit

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+    public bool faceRight = true;
+    public bool isMoving = false;
+
+    public SpriteRenderer sr;
+
+    public void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Input
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        // animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x < 0) {
+            sr.flipX = true;
+        } else if (movement.x > 0) {
+            sr.flipX = false;
+        }
+    }
+
+    // Movement
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
