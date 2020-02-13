3ca7761e732ab8acc2ff05d248ec049233292538
StephenKamali <stephen.kamali@gmail.com>
2020-02-13 02:08:26 -0500

    added basic enemy functionality

---
+++ Assets/Scripts/EnemyScripts/EnemyChildDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChildDelegate : MonoBehaviour
+{
+    private Enemy parent;
+    private string childTag;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (parent != null && childTag != null && collision.tag.Equals("Player"))
+            parent.OnChildTrigger(childTag, collision, Enemy.TriggerType.Enter);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (parent != null && childTag != null && collision.tag.Equals("Player"))
+            parent.OnChildTrigger(childTag, collision, Enemy.TriggerType.Exit);
+    }
+
+    public Enemy Parent
+    {
+        set { parent = value; }
+    }
+
+    public string ChildTag
+    {
+        set { childTag = value; }
+    }
+}
