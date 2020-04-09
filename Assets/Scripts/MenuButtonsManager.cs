3d7d10bed22fd0231700a82c8a877e6a8b78eb3d
Semin Choi <schoi369@gatech.edu>
2020-04-09 21:39:16 +0900

    Some edits on the backgrounds

---
+++ Assets/Scripts/MenuButtonsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonsManager : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("_Level1");
+    }
+
+    public void LoadCredits() {
+        SceneManager.LoadScene("_Credits");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
