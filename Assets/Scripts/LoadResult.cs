cbd3b547777ef6c4025a4fb6d253aa5b62d1d8e0
Semin Choi <schoi369@gatech.edu>
2020-04-11 00:39:36 +0900

    Loads result scene. Score calc not implemented.

---
+++ Assets/Scripts/LoadResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadResult : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene("GoodEnd", LoadSceneMode.Single);
+        }
+    }
+}
