f1cbfc326a247594a6c00fb60be1b5be087d82c8
Semin Choi <schoi369@gatech.edu>
2020-04-11 04:02:57 +0900

    Tutorials (Premise & control explanation added)

--- Assets/Scripts/MenuButtonsManager.cs
+++ Assets/Scripts/MenuButtonsManager.cs
@@ -20,4 +20,28 @@ public class MenuButtonsManager : MonoBehaviour
     public void LoadMainMenu() {
         SceneManager.LoadScene("Main Menu");
     }
+
+    public void LoadTutorial1() {
+        SceneManager.LoadScene("Tutorial 1");
+    }
+
+    public void LoadTutorial2() {
+        SceneManager.LoadScene("Tutorial 2");
+    }
+
+    public void LoadTutorial3() {
+        SceneManager.LoadScene("Tutorial 3");
+    }
+
+    public void LoadTutorial4() {
+        SceneManager.LoadScene("Tutorial 4");
+    }
+
+    public void LoadTutorial5() {
+        SceneManager.LoadScene("Tutorial 5");
+    }
+
+    public void LoadTutorial6() {
+        SceneManager.LoadScene("Tutorial 6");
+    }
 }
